2aa498b2066e2f9eddc819fe0d6732b2e0b8e858
Matvey2-28 <matveyushka.manin@yandex.ru>
2025-10-22 17:52:04 +0200

    доработка

--- test5.py
+++ test5.py
@@ -1,14 +1,16 @@
 a = int(input('Введите первое число: '))
 
 b = int(input('Введите второе число: '))
-
-if a>b:
+if b==0:
+    print('На ноль не делят')
+    
+elif a%b==0:
     print('Делится')
     print(a%b)
     
-if a<b:
+else:
     print('Не делится')
+    print(a/b)
         
-if b==0:
-    print('На ноль не делят')
+
     
