222c80e1f3c91198ace1c541c22fc9c43957aa3e
Matvey2-28 <matveyushka.manin@yandex.ru>
2025-11-14 20:29:20 +0200

    сделал архитектуру доп задания по ооп

---
+++ task_1_dop_oop.py
@@ -0,0 +1,28 @@
+class Players:
+    
+    level = 0
+    
+    def __init__(self, username):
+        self.username = username
+        varrior_list = {}
+        
+    def create_varriors(self, varriors):
+        self.varrior = varrior
+        for i in range(len(varriors)):
+            self.varrior_list[varriors[i]] = {}
+            
+    def complete_task(self):
+        self.level += 1
+        print('Воин выполнил поручение от героя')
+        
+            
+    
+            
+
+class Varrior:
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def go_ahead(self):
+        
