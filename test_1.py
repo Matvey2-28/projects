2205f3ebe685e46ed9c844f9cce7302c8ffe7dc2
Matvey2-28 <matveyushka.manin@yandex.ru>
2025-11-19 17:01:29 +0200

    изменения

--- test_1.py
+++ test_1.py
@@ -3,6 +3,7 @@ import numpy as np
 a = [52, 67, 42]
 b = [23, 12, 99]
 c = [45, 74, 24]
+d = np.random.uniform(0, 1, 100)
 
 a1 = np.array(a)
 b1 = np.array(b)
@@ -10,3 +11,4 @@ c1 = np.array(c)
 
 print(max(max(a1), max(b1), max(c1)))
 print(sum(a1) + sum(b1) + sum(c1))
+print(d)
