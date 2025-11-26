28a4aa2c06bcd7900f2b3a9700404c2a1c882239
Matvey2-28 <matveyushka.manin@yandex.ru>
2025-11-26 16:37:17 +0200

    изменение

--- test_4.py
+++ test_4.py
@@ -1,4 +1,7 @@
+import random as rd
+
 flowers = ['Розы', 'Пионы', 'Тюльпаны']
 colors = ['красный', 'синий', 'оранжевый', 'желтый', 'зеленый']
+color_random = [rd.choice(colors) for i in range(len(flowers))]
 
-print(dict(zip(flowers, colors)))
+print(dict(zip(flowers, color_random)))
--- test_5.py
+++ test_5.py
@@ -1,7 +1,6 @@
 fio = 'MALIK MATVEY GEORGIEVICH'
-fio1 = 'malik matvey georgievich'
 
 fio_codes = [ord(symbol) for symbol in fio]
-fio1_codes = [ord(symbol) for symbol in fio1]
+fio1_codes = [ord(symbol) for symbol in fio.lower()]
 
 print(sum(fio_codes) + sum(fio1_codes))
