0586b82943f0a5af0c2822e74a5de4f60c5d2244
Matvey2-28 <matveyushka.manin@yandex.ru>
2025-11-26 18:18:43 +0200

    сделали 6 урок

--- lec_beautyplot.py
+++ lec_beautyplot.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 x = [3, 8, 5]
 y = [7, 4, 9]
 
-#plt.xkcd() #Сделать график корявым
-
 plt.plot(x, y, color='g', label='Graf 1', marker='>', ms=5)
 plt.plot(y, x, color='r', label='Graf 2', marker='o', ms=3)
 
@@ -14,4 +12,3 @@ plt.legend()
 plt.title('Base')
 plt.grid()
 plt.savefig('fig_2.png')
-plt.close()
