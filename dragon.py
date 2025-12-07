5fe3964b87de418d4ad04b8ba8d9216ca0c190b5
Matvey2-28 <matveyushka.manin@yandex.ru>
2025-12-07 14:43:15 +0200

    еще изменения

--- dragon.py
+++ dragon.py
@@ -1,5 +1,5 @@
-import time
-from hero import Hero
+# import time
+# from hero import Hero
 
 
 class Dragon:
@@ -9,24 +9,4 @@ class Dragon:
         self.dragon_hp = 5000
         self.dragon_dm = 100
         self.hl = 300
-    
-    def heal(self):
-        self.hero1_hp = 0
-        if hero.hero_hp < 15000:
-            while hero.hero_hp < 15000:
-                time.sleep(1)
-                hero.hero_hp += self.hl
-                self.hero1_hp = hero.hero_hp
-                print(f'Герой восстанавливает здоровье: {hero.hero_hp} + {self.hl}')
-                if hero.hero_hp >= 15000:
-                    print(f'Герой восстановил здоровье: {self.hero1_hp}')
-                    break
-                    
-        elif hero.hero_hp == 15000:
-            print('Герой не нуждается в лечении')
-            
-            
-            
-            
 
-hero = Hero('Makanchik')
--- hero.py
+++ hero.py
@@ -1,14 +1,14 @@
-# import time
-# from npc import NPC 
-# from dragon import Dragon
-# from skeleton import Skeleton
+import time
+import random as rd
+from npc import NPC 
+from dragon import Dragon
+from skeleton import Skeleton
 
 class Hero:
                     
     def __init__(self, name):
         self.name = name
         self.hero_hp = 14000
-        self.hero_dm = 300
                    
                            
     def attack_with_dragon(self):
@@ -16,8 +16,8 @@ class Hero:
         self.deaths_count_with_dragon = 0
         while npc.npc_hp > 0:
             time.sleep(1)
-            npc.npc_hp -= self.hero_dm + dragon1.dragon_dm
-            print(f'Здоровье врага: {npc.npc_hp} - {self.hero_dm + dragon1.dragon_dm}')
+            npc.npc_hp -= rd.randrange(100, 500, 50) + dragon1.dragon_dm
+            print(f'Здоровье врага: {npc.npc_hp} - {rd.randrange(100, 500, 50) + dragon1.dragon_dm}')
                 
             if npc.npc_hp <= 0:
                 self.kills_count_with_dragon += 1
@@ -31,14 +31,29 @@ class Hero:
         self.deaths_count_with_skelet = 0
         while npc.npc_hp > 0:
             time.sleep(1)
-            npc.npc_hp -= self.hero_dm + skelet1.skelet_dm
-            print(f'Здоровье врага: {npc.npc_hp} - {self.hero_dm + skelet1.skelet_dm}')
+            npc.npc_hp -= rd.randrange(100, 500, 50) + skelet1.skelet_dm
+            print(f'Здоровье врага: {npc.npc_hp} - {rd.randrange(100, 500, 50) + skelet1.skelet_dm}')
             
             if npc.npc_hp <= 0:
                 self.kills_count_with_skelet += 1
                 print('Твой герой одержал победу!')
                 break
                 
+                
+    def healing(self):
+        self.hero1_hp = 0
+        if self.hero_hp < 15000:
+            while self.hero_hp < 15000:
+                time.sleep(1)
+                self.hero_hp += dragon1.hl
+                self.hero1_hp = self.hero_hp
+                print(f'Герой восстанавливает здоровье: {self.hero_hp} + {dragon1.hl}')
+                if self.hero_hp >= 15000:
+                    print(f'Герой восстановил здоровье: {self.hero1_hp}')
+                    break
+                                
+                elif self.hero_hp == 15000:
+                    print('Герой не нуждается в лечении')
                 
                 
     def go_outside(self):
@@ -52,18 +67,18 @@ class Hero:
     def check_hero_statistics(self):
         print('________Статистика Героя________')
         print('Hero:')
-        print('\t Health: ', dragon1.hero1_hp)
+        print('\t Health: ', self.hero1_hp)
         print(f'\t Monsters: {dragon1.dragon_name}, {skelet1.skelet_name}')
-        print('\t Kills: ', self.kills_count_with_dragon + self.kills_count_with_skelet)
-        print('\t Deaths: ', self.deaths_count_with_dragon + self.deaths_count_with_skelet)
+        print('\t Kills: ', hero.kills_count_with_dragon + hero.kills_count_with_skelet)
+        print('\t Deaths: ', hero.deaths_count_with_dragon + hero.deaths_count_with_skelet)
         print('________________________________')
                     
                   
 
-# npc =  NPC('Hog Rider')
-# # hero = Hero('Makanchik')
-# dragon1 = Dragon('Leviathan')
-# skelet1 = Skeleton('Varior')
+npc =  NPC('Hog Rider')
+# hero = Hero('Makanchik')
+dragon1 = Dragon('Leviathan')
+skelet1 = Skeleton('Varior')
 
 
 
