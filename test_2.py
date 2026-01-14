2e8b2d620d0d838c75bbcdc27049bfca68e6962b
Matvey2-28 <matveyushka.manin@yandex.ru>
2026-01-14 15:34:04 +0200

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -1,7 +1,27 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 10 14:15:27 2026
-
-@author: student24
-"""
-
+class Organic:
+    def __init__(self, count_of_kletok):
+        self.data = count_of_kletok
+       
+        
+    def __add__(self, other):
+        return Organic(self.data + other)
+    
+    def __sub__(self, other):
+        return Organic(self.data - other)
+    
+    def __mul__(self, other):
+        return Organic(self.data * other)
+    
+    def __truediv__(self, other):
+        return Organic(self.data // other)
+    
+org = Organic(30)
+result1 = org + 12
+result2 = org - 12
+result3 = org * 2
+result4 = org / 12
+print(result1.data)
+print(result2.data)
+print(result3.data)
+print(result4.data)
+        
