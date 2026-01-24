24b82511a041b87cd9414630bd4942ea2fb3644d
Matvey2-28 <matveyushka.manin@yandex.ru>
2026-01-24 11:45:42 +0200

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -1,7 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 17 14:43:11 2026
+import random as rd
 
-@author: student24
-"""
-
+def changer(n):
+    i = rd.choice(rd.randrange(10, 1000))
+    n = int(input('Введите ваше число: '))
+    print('Число корректно')
+    print('Начинаю угадывать ваше число')
+    print(f'Число меньше {i}?')
+    a = int(input())
+    if a = int(input('Да')):
+        i = i // 2
+    elif a = int(input('Нет')):
+        
+        
+    
