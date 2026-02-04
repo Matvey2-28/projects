9f8570ed87c858f6cbf28ed911884d0c1097d00a
Matvey2-28 <matveyushka.manin@yandex.ru>
2026-02-04 16:44:47 +0200

    добавление

--- create.py
+++ create.py
@@ -1,44 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as plt3d
 import h5py
 import sys
 
+
 AE = 149597870700
-N = 10000
+N = 1000
 M_SUN = 1.998e30
 G = 6.67e-11
 
-masses = np.full(N+1, 1e17)
+masses = np.full(N, 1e17)
 id_parts = np.arange(0, N, 1)
 
-phi = np.linspace(0, np.pi*2, N)
-R = np.random.random(N) * 1.2 + 2.01
+phi = np.linspace(0, 2*np.pi*6, N)
+r = np.random.random(N) * 1.2 + 2.01
 
-x = R * np.cos(phi)
-y = R * np.sin(phi)
+x = r * np.cos(phi)
+y = r * np.sin(phi)
 z = np.zeros(N)
-
 coords = np.zeros((N, 3))
 coords[:, 0], coords[:, 1] = x, y
 
-v = np.sqrt(G * M_SUN / R)
-vx = v * np.cos(phi)
-vy = v * np.sin(phi)
+v = np.sqrt(G * M_SUN / r)
+v_x = v * np.cos(phi)
+v_y = v * np.sin(phi)
 
 vel = np.zeros((N, 3))
-vel[:, 0], vel[:, 1] = vx, vy
+vel[:, 0], vel[:, 1] = v_x, v_y
 
+#Sun
 masses[0] = M_SUN
 coords[0] = [0, 0, 0]
-vel[0] = [0, 0, 0]
+coords = (coords + 3.3) * AE
+# print(coords)
 
-# plt.plot(coords[:, 0], coords[:, 1], 'o')
+# plt.plot(coords[:, 0], coords[:, 1] , 'o', color='#FF3838')
 # plt.axis('equal')
-# plt.savefig('sun_system.png', dpi=1000)
+# plt.savefig('Solor_sys.png', dpi=1000)
+# plt.close()
 
 
 # File
-file = h5py.File('./IC9.hdf5', "w")
+
+file = h5py.File('./IC.hdf5', "w")
 
 # Header
 grp = file.create_group("/Header")
@@ -54,8 +59,8 @@ grp.attrs["Dimension"] = 3
 
 # Units
 grp = file.create_group("/Units")
-grp.attrs["Unit length in cgs (U_L)"] = 1.0
-grp.attrs["Unit mass in cgs (U_M)"] = 1.0
+grp.attrs["Unit length in cgs (U_L)"] = 100
+grp.attrs["Unit mass in cgs (U_M)"] = 1000
 grp.attrs["Unit time in cgs (U_t)"] = 1.0
 grp.attrs["Unit current in cgs (U_I)"] = 1.0
 grp.attrs["Unit temperature in cgs (U_T)"] = 1.0
@@ -63,9 +68,10 @@ grp.attrs["Unit temperature in cgs (U_T)"] = 1.0
 
 # Particle group
 grp = file.create_group("/PartType1")
+
 ds = grp.create_dataset("Velocities", (N, 3), "f", data=vel)
 ds = grp.create_dataset("Masses", (N, 1), "f", data=masses)
-ds = grp.create_dataset("ParticleIDs", (N, 1), "L", data=id_parts)
+ds = grp.create_dataset("ParticleIDs", (N, 1), "L", data = id_parts)
 ds = grp.create_dataset("Coordinates", (N, 3), "d", data=coords)
 
 file.close()
