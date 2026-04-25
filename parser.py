fe36926a44d76d9d4273297db18ccb4b185c3588
Matvey2-28 <matveyushka.manin@yandex.ru>
2026-04-25 14:33:21 +0200

    changes

--- parser.py
+++ parser.py
@@ -8,29 +8,19 @@ class Parser:
             print('Файл загружен.', '\n')
             self.data = json.load(data_file)
 
-    def get_visual_settings(self):
-        print('Получены визуальные настройки.')
-        data = self.data['visual_settings']
+    def get_physical_settings(self):
+        print('Получены физические настройки.')
+        data = self.data['phys_settings']
         visual_settings = []
         for parametr in data:
             visual_settings.append(data[parametr]['value'])
         print(visual_settings, '\n')
         return visual_settings
 
-
-    def get_math_settings(self):
-        print('Получены математические настройки.')
-        data = self.data['math_settings']
-        visual_settings = []
-        for parametr in data:
-            visual_settings.append(data[parametr]['value'])
-        print(visual_settings, '\n')
-        return visual_settings
 
 
 if __name__ == '__main__':
     parser = Parser('input.json')
-    a = parser.get_math_settings()
-    b = parser.get_visual_settings()
+    a = parser.get_physical_settings()
     print()
         
